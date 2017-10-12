261cd7436b803735e55a3090e24192b540f1934e
Unknown <daniel.jaramillo7@udea.edu.co>
2017-10-12 01:00:14 -0500

    Métodos agragados
    Agregados unos métodos que ayudan a algunas funcionalidades. Métodos agregados:
    
    - Produccion getProduccion(int i) :: obtiene la producción número i de la gramática
    
    - ArrayList<Produccion> getProduccionesIniciales() :: obtiene las producciones iniciales de la gramática. Las producciones iniciales son las producciones que contienen en el lado izquierdo de la producción al no terminal inicial (normalmente <S>)
    
    Además, se creó la estructura del método getProduccionesInalcanzables(), pero aún no es funcional.

--- src/Model/Gramatica.java
+++ src/Model/Gramatica.java
@@ -18,7 +18,7 @@ import java.util.logging.Logger;
  */
 public class Gramatica {
     
-    ArrayList<Produccion> producciones;
+    private final ArrayList<Produccion> producciones;
     
     /**
      * @param args the command line arguments
@@ -35,12 +35,19 @@ public class Gramatica {
             for (String str : terminales) {
                 System.out.println(str);
             }
+            
+            
+            System.out.println("No terminal inicial: " + gramatica.getNoTerminalInicial());
+            
+            
+            System.out.println("Producciones iniciales:");
+            for (Produccion p : gramatica.getProduccionesIniciales()) {
+                System.out.println(p.getProduccion());
+            }
         } catch (FileNotFoundException ex) {
             System.out.println("------------------ Archivo no encontrado ------------------");
             Logger.getLogger(Gramatica.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
-        
     }
 
     public ArrayList<Produccion> getProducciones() {
@@ -63,6 +70,42 @@ public class Gramatica {
         }
     }
     
+    public Produccion getProduccion(int i){
+        return producciones.get(i);
+    }
     
+    public String getNoTerminalInicial(){
+        Expresion expresionInicial = producciones.get(0).getLadoIzq();
+        TreeSet noTerminalesIniciales = expresionInicial.getNoTerminales();
+        return (String)noTerminalesIniciales.first();
+    }
     
+    public ArrayList<Produccion> getProduccionesIniciales(){
+        
+        ArrayList<Produccion> produccionesIniciales = new ArrayList<>();
+        produccionesIniciales.add(getProduccion(0));
+        
+        String noTerminalInicial = getNoTerminalInicial();
+        int cantidadProducciones = producciones.size();
+        
+        //se comienza desde la producción 2, porque la producción 1 POR DEFECTO es la que contiene al no terminal inicial en su expresión izquieda
+        for (int i = 1; i < cantidadProducciones; i++) {
+            Produccion produccion = getProduccion(i);
+            Expresion expresion = produccion.getLadoIzq();
+            if (expresion.getExpresion().compareTo(noTerminalInicial) == 0) {
+                produccionesIniciales.add(produccion);
+            }
+        }
+        return produccionesIniciales;
+    }
+    
+    public ArrayList<Produccion> getProduccionesInalcanzables(){ 
+        ArrayList<Produccion> produccionesInalcanzables = new ArrayList<>();
+        //int cantidadProducciones = producciones.size();
+        TreeSet conjuntoProduccionesInalcanzables = new TreeSet(producciones);
+        
+        
+        
+        return null;
+    }
 }
