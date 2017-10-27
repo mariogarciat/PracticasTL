541851789e3e3b79024c935678459f8f372991b7
Unknown <daniel.jaramillo7@udea.edu.co>
2017-10-26 19:17:01 -0500

    Funciones agregadas
    Las principales funciones agregadas son:
    
    - getTerminales_noTerminales_() :: es un método privado que obtiene los terminales y no terminales de una gramática. Es privado porque es llamado desde el constructor de la clase Gramatica.
    
    -  getProduccionesIniciales() :: obtiene todas las producciones cuyo lado izquierdo son el no terminal inicial
    
    -  getNoTerminalesInalcanzables() :: obtiene todos los no terminales inalcanzables que hay en la gramática

--- src/Model/Expresion.java
+++ src/Model/Expresion.java
@@ -1,18 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Model;
 
 import java.util.TreeSet;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-/**
- *
- * @author Daniel Jaramillo
- */
 public class Expresion {
 
     private final String expresion;
--- src/Model/Gramatica.java
+++ src/Model/Gramatica.java
@@ -1,28 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Model;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Stack;
 import java.util.TreeSet;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- *
- * @author Daniel Jaramillo
- */
 public class Gramatica {
-    
+
     private final ArrayList<Produccion> producciones;
-    
-    /**
-     * @param args the command line arguments
-     */
+    private final TreeSet<String> terminales;
+    private final TreeSet<String> noTerminales;
+
     public static void main(String[] args) {
         Gramatica gramatica;
         try {
@@ -35,14 +26,20 @@ public class Gramatica {
             for (String str : terminales) {
                 System.out.println(str);
             }
-            
-            
+
             System.out.println("No terminal inicial: " + gramatica.getNoTerminalInicial());
-            
-            
+
             System.out.println("Producciones iniciales:");
             for (Produccion p : gramatica.getProduccionesIniciales()) {
                 System.out.println(p.getProduccion());
+            }
+
+            System.out.println("");
+            System.out.println("No termiales INALCANZABLES:");            
+            TreeSet noTerminalesIn = gramatica.getNoTerminalesInalcanzables();
+            for (Object str : noTerminalesIn) {
+                String s = (String)str;
+                System.out.println(s);
             }
         } catch (FileNotFoundException ex) {
             System.out.println("------------------ Archivo no encontrado ------------------");
@@ -50,18 +47,46 @@ public class Gramatica {
         }
     }
 
+    private void getTerminales_noTerminales_() {
+        int numeroProducciones = producciones.size();
+        for (int i = 0; i < numeroProducciones; i++) {
+            Produccion produccion_i = getProduccion(i);
+            Expresion expresion_izquierda_i = produccion_i.getLadoIzq();
+            Expresion expresion_derecha_i = produccion_i.getLadoDer();
+            terminales.addAll(expresion_izquierda_i.getTerminales());
+            terminales.addAll(expresion_derecha_i.getTerminales());
+            noTerminales.addAll(expresion_izquierda_i.getNoTerminales());
+            noTerminales.addAll(expresion_derecha_i.getNoTerminales());
+        }
+    }
+
+    public TreeSet getNoTerminales() {
+        return noTerminales;
+    }
+
+    public TreeSet getTerminales() {
+        return terminales;
+    }
+
     public ArrayList<Produccion> getProducciones() {
         return producciones;
     }
-    
-    public Gramatica(File file) throws FileNotFoundException{
+
+    public Gramatica(File file) throws FileNotFoundException {
         producciones = control.Txt_gramatica_parser.getProductionArrayList(file);
+        terminales = new TreeSet<>();
+        noTerminales = new TreeSet<>();
+        getTerminales_noTerminales_();
+
     }
 
     public Gramatica(String path) throws FileNotFoundException {
         producciones = control.Txt_gramatica_parser.getProductionArrayList(path);
+        terminales = new TreeSet<>();
+        noTerminales = new TreeSet<>();
+        getTerminales_noTerminales_();
     }
-    
+
     void mostrarGramatica() {
         int i = 1;
         for (Produccion produccion : producciones) {
@@ -69,43 +94,90 @@ public class Gramatica {
             i++;
         }
     }
-    
-    public Produccion getProduccion(int i){
+
+    public Produccion getProduccion(int i) {
         return producciones.get(i);
     }
-    
-    public String getNoTerminalInicial(){
+
+    public String getNoTerminalInicial() {
         Expresion expresionInicial = producciones.get(0).getLadoIzq();
         TreeSet noTerminalesIniciales = expresionInicial.getNoTerminales();
-        return (String)noTerminalesIniciales.first();
+        return (String) noTerminalesIniciales.first();
     }
-    
-    public ArrayList<Produccion> getProduccionesIniciales(){
-        
-        ArrayList<Produccion> produccionesIniciales = new ArrayList<>();
-        produccionesIniciales.add(getProduccion(0));
-        
-        String noTerminalInicial = getNoTerminalInicial();
-        int cantidadProducciones = producciones.size();
-        
-        //se comienza desde la producción 2, porque la producción 1 POR DEFECTO es la que contiene al no terminal inicial en su expresión izquieda
-        for (int i = 1; i < cantidadProducciones; i++) {
+
+    //obtiene las producciones que comienzan con el no terminal ingresado como noTerminal
+    //NOTA: también deben escribirse los corchetes angulares
+    public ArrayList<Produccion> getProducciones(String noTerminal) {
+
+        ArrayList<Produccion> listaProducciones = new ArrayList<>();
+        int cantidadProducciones = this.producciones.size();
+
+        for (int i = 0; i < cantidadProducciones; i++) {
             Produccion produccion = getProduccion(i);
             Expresion expresion = produccion.getLadoIzq();
-            if (expresion.getExpresion().compareTo(noTerminalInicial) == 0) {
-                produccionesIniciales.add(produccion);
+            if (expresion.getExpresion().compareTo(noTerminal) == 0) {
+                listaProducciones.add(produccion);
             }
         }
-        return produccionesIniciales;
+        return listaProducciones;
     }
-    
-    public ArrayList<Produccion> getProduccionesInalcanzables(){ 
+
+    //obtiene todas las producciones cuyo lado izquierdo son el no terminal inicial
+    public ArrayList<Produccion> getProduccionesIniciales() {
+        return getProducciones(getNoTerminalInicial());
+    }
+
+    public TreeSet getNoTerminalesInalcanzables() {
+
+        // el conjunto noTerminalesAlcanzables inicialmente debe contener al no terminal inicial
+        TreeSet noTerminalesAlcanzables = new TreeSet();
+        noTerminalesAlcanzables.add(this.getNoTerminalInicial());
+
+        // se van a ir agregando, a medida que se encuentran, producciones cuyo lado izquierdo son no terminales alcanzables.
+        // Sólo se agregan a la pila si no hacen parte del conjunto noTerminalesAlcanzables.
+        // Inicialmente contine las producciones iniciales (producciones cuyo lado izquierdo son el no terminal inicial)
+        Stack<Produccion> produccionesAlcanzables = new Stack<>();
+        ArrayList<Produccion> produccionesIniciales = getProduccionesIniciales();
+        for (Produccion produccionInicial : produccionesIniciales) {
+            produccionesAlcanzables.push(produccionInicial);
+        }
+
+        while (!produccionesAlcanzables.empty()) {
+            // se desapila y analiza la producción que está en el tope de la pila
+            //de producciones alcanzables
+            Produccion p = produccionesAlcanzables.pop();
+
+            // se encuentran TODOS los no terminales en el lado derecho de la producción en cuestión
+            TreeSet nT = p.getLadoDer().getNoTerminales();
+
+            // para cada no terminal encontrado es necesario hacer lo siguiente
+            for (Object o : nT) {
+                String noTerminal = (String) o;
+
+                //SOLO si el no terminal aún no pertenece al conjunto de no terminales que se saben alcanzables
+                //entonces hacer lo siguiente
+                if (!noTerminalesAlcanzables.contains(o)) {
+                    // agregarlos al conjunto de no terminales que se saben alcanzables
+                    noTerminalesAlcanzables.add(o);
+                    //agregar a la pila de producciones alcanzables todas las producciones cuyo lado
+                    //izquierdo contienen el no terminal que se sabe alcanzable 
+                    ArrayList<Produccion> array = getProducciones(noTerminal);
+                    for (Produccion produccion : array) {
+                        produccionesAlcanzables.push(produccion);
+                    }
+                }
+            }
+        }
+        TreeSet noTerminalesInalcanzables = (TreeSet) getNoTerminales().clone();
+        noTerminalesInalcanzables.removeAll(noTerminalesAlcanzables);
+        return noTerminalesInalcanzables;
+    }
+
+    public ArrayList<Produccion> getProduccionesInalcanzables() {
         ArrayList<Produccion> produccionesInalcanzables = new ArrayList<>();
         //int cantidadProducciones = producciones.size();
         TreeSet conjuntoProduccionesInalcanzables = new TreeSet(producciones);
-        
-        
-        
+
         return null;
     }
 }
