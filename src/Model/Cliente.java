27deea5f0c01a161d3798886a17d25617366f320
Unknown <daniel.jaramillo7@udea.edu.co>
2017-10-30 13:18:03 -0500

    Terminado el método para convertir gramáticas
    El método convierte gramáticas lineales por derecha a gramáticas especiales

--- src/Model/Cliente.java
+++ src/Model/Cliente.java
@@ -2,16 +2,23 @@ package Model;
 
 import control.LinealPorDerechaToEspecialParser;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
+import java.util.TreeSet;
 
 public class Cliente {
     public static void main(String[] args) throws FileNotFoundException {
         Gramatica gramatica = new Gramatica("gramatica_lineal_derecha2.txt");
         gramatica.mostrarGramatica();
-        System.out.println("");
+        System.out.println("Es especial: " + gramatica.esEspecial());
+        TreeSet<String> noTerminales = gramatica.getNoTerminales();
+        for (String noTerminal : noTerminales) {
+            System.out.println(noTerminal);
+        }
+        
+        
+       
         System.out.println("");
         System.out.println("");
         Gramatica gramaticaEspecial = LinealPorDerechaToEspecialParser.convertir(gramatica);
-        gramaticaEspecial.mostrarGramatica();
+        gramaticaEspecial.mostrarGramatica();       
     }
 }
